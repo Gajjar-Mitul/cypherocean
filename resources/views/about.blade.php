681d1e3189ad83af44a5d277f93b0df6879f4dd1
lomas <lomasbakraniya@gmail.com>
2021-03-15 20:41:35 +0530

    loms

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -29,8 +29,8 @@
           <div class="col-12">
             <div class="row justify-content-center">
               <div class="col-md-7 text-center hero-text">
-                <h1 data-aos="fade-up" data-aos-delay="">SoftLand Features</h1>
-                <p class="mb-5" data-aos="fade-up" data-aos-delay="100">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                <h1 data-aos="fade-up" data-aos-delay="">About Us</h1>
+                <p class="mb-5" data-aos="fade-up" data-aos-delay="100">We deliver world-class development services</p>
               </div>
             </div>
           </div>
@@ -45,9 +45,9 @@
       <div class="container">
         <div class="row align-items-center">
           <div class="col-md-4 mr-auto">
-            <h2 class="mb-4">Seamlessly Communicate</h2>
-            <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur at reprehenderit optio, laudantium eius quod, eum maxime molestiae porro omnis. Dolores aspernatur delectus impedit incidunt dolore mollitia esse natus beatae.</p>
-            <p><a href="#">Read More</a></p>
+            <h2 class="mb-4">About CypherOcean</h2>
+            <p class="mb-4">Trusted fully managed dedicated software development teams in India, we deliver world-class custom software development projects on demand for our clients worldwide.</p>
+            <p class="mb-4">Our purpose is to create life-changing opportunities for those we serve. We are a team and we take responsibility for the success of everything we do.</p>
           </div>
           <div class="col-md-6" data-aos="fade-left">
             <img src="assets/img/undraw_svg_2.svg" alt="Image" class="img-fluid">
@@ -60,9 +60,14 @@
       <div class="container">
         <div class="row align-items-center">
           <div class="col-md-4 ml-auto order-2">
-            <h2 class="mb-4">Gather Feedback</h2>
-            <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur at reprehenderit optio, laudantium eius quod, eum maxime molestiae porro omnis. Dolores aspernatur delectus impedit incidunt dolore mollitia esse natus beatae.</p>
-            <p><a href="#">Read More</a></p>
+            <h2 class="mb-4">Values-driven</h2>
+			<ul>
+				<li><h5 class="mb-4">Research, design, & build</h5></li>
+				<li><h5 class="mb-4">Transparent agreements</h5></li>
+				<li><h5 class="mb-4">Culture of respect</h5></li>
+				<li><h5 class="mb-4">Value of reputation</h5></li>
+				<li><h5 class="mb-4">Protection of confidentiality</h5></li>
+			</ul> 		                   
           </div>
           <div class="col-md-6" data-aos="fade-right">
             <img src="assets/img/undraw_svg_3.svg" alt="Image" class="img-fluid">
@@ -75,30 +80,22 @@
       <div class="container">
         <div class="row align-items-center">
           <div class="col-md-4 mr-auto">
-            <h2 class="mb-4">Present Designs Inspiration</h2>
-            <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur at reprehenderit optio, laudantium eius quod, eum maxime molestiae porro omnis. Dolores aspernatur delectus impedit incidunt dolore mollitia esse natus beatae.</p>
-            <p><a href="#">Read More</a></p>
+            <h2 class="mb-4">Our Principles</h2>
+            
+            <p>Building a strong relationship with our clients</p>
+			<p>We take responsibility and build around the goals, rather than tasks</p>
+			<p>Making mistakes is acceptable, learning from them is not</p>
+			<p>We are not box builders, we create experiences and solutions</p>
+			<p>Our people is the core of everything we do</p>
           </div>
           <div class="col-md-6" data-aos="fade-left">
-            <img src="assets/img/undraw_svg_3.svg" alt="Image" class="img-fluid">
+            <img src="assets/img/undraw_svg_4.svg" alt="Image" class="img-fluid">
           </div>
         </div>
       </div>
     </section>
 
-    <section class="section">
-      <div class="container">
-        <div class="row align-items-center">
-          <div class="col-md-4 ml-auto order-2">
-            <h2 class="mb-4">Powerful App Design </h2>
-            <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur at reprehenderit optio, laudantium eius quod, eum maxime molestiae porro omnis. Dolores aspernatur delectus impedit incidunt dolore mollitia esse natus beatae.</p>
-            <p><a href="#">Read More</a></p>
-          </div>
-          <div class="col-md-6" data-aos="fade-right">
-            <img src="assets/img/undraw_svg_4.svg" alt="Image" class="img-fluid">
-          </div>
-        </div>
-      </div>
+    <section class="section">     
     </section>
    
 @endsection
